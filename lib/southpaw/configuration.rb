06e3d593747b856c16bedd55f28a270694feb86e
Mike Williamson <mike@korora.ca>
2014-07-28 15:47:47 -0400

    Ensure methods are defined on config instance
    Also we are capturing *args and only keeping the first one.

--- lib/southpaw/configuration.rb
+++ lib/southpaw/configuration.rb
@@ -15,15 +15,15 @@ module Southpaw
     private
 
     def define_method_missing
-      self.class.send :define_method, :method_missing do |meth, args|
-        raise_exception_for_nil_args meth.to_sym, args
+      eigenclass.__send__ :define_method, :method_missing do |meth, *args|
+        raise_exception_for_nil_args meth.to_sym, args[0]
         eigenclass.send :attr_reader, meth.to_sym
-        instance_variable_set "@#{meth}".to_sym, args
+        instance_variable_set "@#{meth}".to_sym, args[0]
       end
     end
 
     def undefine_method_missing
-      self.class.send :undef_method, :method_missing
+      eigenclass.__send__ :undef_method, :method_missing
     end
 
 
